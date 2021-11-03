89b9bbd164c01789ba9a961573e5d70a705153b7
Sabelius <sbebby92@virgilio.it>
2021-11-03 15:14:19 +0100

    first commit

---
+++ app/Http/Controllers/homeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\movie;
+
+use Illuminate\Http\Request;
+
+class homeController extends Controller
+{
+    public function index(){
+
+        $movies = movie::all();
+        dd($movies);
+
+
+        $data = [
+            "movies" => $movies
+        ];
+
+        return view("home", $data);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section("title", "Movies-List")
+
+
+@section("content")
+
+
+
+@endsection
