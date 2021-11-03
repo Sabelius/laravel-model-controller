2113b5acdcff97aa869447770a22aa2d8ad9626b
Sabelius <sbebby92@virgilio.it>
2021-11-03 16:11:02 +0100

    aggiornamento card movies

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,15 +8,15 @@
 <h1 class="text-center text-uppercase">Movies:</h1>
 @foreach ($movies as $movie)
 
-<div class="card container">
+<div class="card container col-4 mt-3">
     <div class="card-body">
-      <h5 class="card-title text-uppercase">{{$movie->title}}</h5>
-      <h6 class="card-original-title mb-2 text-muted">{{$movie->original_title}}</h6>
-      <h6 class="card-text">{{$movie->nationality}}</h6>
-      <h6 class="card-text">{{$movie->date}}</h6>
-      <h6 class="card-text">{{$movie->vote}}</h6>
+      <h5 class="card-title text-uppercase text-center">{{$movie->title}}</h5>
+      <h6 class="card-original-title mb-2 text-muted text-center">{{$movie->original_title}}</h6>
+      <h6 class="card-text text-capitalize text-center">{{$movie->nationality}}</h6>
+      <h6 class="card-text text-center">{{$movie->date}}</h6>
+      <h6 class="card-text text-center">{{$movie->vote}}</h6>
     </div>
-  </div>
+</div>
     
 @endforeach
 @endsection
